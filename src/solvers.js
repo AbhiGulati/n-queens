900b7e5c900ee70a6d0e15404f7f4d29c2f8f81d
MKS-students <mks.students@hackreactor.com>
2015-06-17 14:47:38 -0500

    finding a Rooks solution

--- src/solvers.js
+++ src/solvers.js
@@ -14,10 +14,28 @@
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  var solution = new Board({ n : n});
 
+  var fillRowWithRook = function(r, board) {
+    for(var i = 0; i < n; i++) {
+      board.togglePiece(r,i);
+      if(!(board.hasAnyRowConflicts() || board.hasAnyColConflicts())) {
+        if(r === n-1) {
+          return board;
+        } else {
+          return fillRowWithRook(r+1, board);
+        }
+      } else {
+        board.togglePiece(r, i)
+      }
+    }
+
+    return undefined;
+  }
+
+  solution = fillRowWithRook(0, solution);
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+  return solution.rows();
 };
 
 
